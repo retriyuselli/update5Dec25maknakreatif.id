7eab0ea6d0f06b06eb313d4f078ac44e39afb60a
Makna Kreatif Team <retriyuselli@maknakreatif.id>
2025-12-06 01:14:41 +0700

    update role

--- resources/views/front/auth/login.blade.php
+++ resources/views/front/auth/login.blade.php
@@ -79,15 +79,6 @@
                                         Sign in with Google
                                     </a>
                                 @endif
-                                @if (Route::has('front.register'))
-                                    <a href="{{ route('front.register') }}"
-                                        class="w-full inline-flex items-center justify-center px-4 py-2 rounded-lg border border-gray-300 text-gray-700 hover:bg-gray-50">Create
-                                        an account</a>
-                                @else
-                                    <a href="{{ route('pendaftaran') }}"
-                                        class="w-full inline-flex items-center justify-center px-4 py-2 rounded-lg border border-gray-300 text-gray-700 hover:bg-gray-50">Create
-                                        an account</a>
-                                @endif
                             </div>
                         </form>
                     </div>
