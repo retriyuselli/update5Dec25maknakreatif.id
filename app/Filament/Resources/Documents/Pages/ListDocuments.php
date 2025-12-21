2a9a1e429ad182f1b2cec48171c7533f0a0ab190
Makna Kreatif Team <retriyuselli@maknakreatif.id>
2025-12-21 23:53:38 +0700

    update

--- app/Filament/Resources/Documents/Pages/ListDocuments.php
+++ app/Filament/Resources/Documents/Pages/ListDocuments.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\Documents\Pages;
 use App\Filament\Resources\Documents\DocumentResource;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Support\Facades\Auth;
 
 class ListDocuments extends ListRecords
 {
@@ -13,7 +14,8 @@ class ListDocuments extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            CreateAction::make(),
+            CreateAction::make()
+                ->visible(fn () => Auth::user()?->hasRole('super_admin')),
         ];
     }
 }
